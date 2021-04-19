e44b4a54b195f6f6d4fb5d0af7580aa9643729e5
Daniel <danieldiazgonza52@gmail.com>
2021-04-19 15:09:04 -0500

    Added project structure

---
+++ asistance/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from asistance.users.models import User
+from asistance.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
