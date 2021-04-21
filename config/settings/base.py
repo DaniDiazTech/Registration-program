c746cd005d494fe7a6a0b3ee82d02f3ae0fa7f0c
Daniel <danieldiazgonza52@gmail.com>
2021-04-21 18:13:51 -0500

    {Modified} Added classroom app

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,6 +76,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "asistance.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "asistance.classrooms.apps.ClassroomsAppConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
